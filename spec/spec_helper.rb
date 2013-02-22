b98a38a33276f8a5d2aeefc01457af2a9bed36aa
Matt Wigham <matt@bigcartel.com>
2013-02-21 20:07:07 -0700

    Fix deprecated RSpec config thing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,7 @@ require 'dugway'
 RSpec.configure do |config|
   config.add_setting :fixture_path
   config.fixture_path = File.join(Dir.pwd, 'spec', 'fixtures')
-end
-
-Spec::Runner.configure do |config|
+  
   config.before(:each) do
     stub_request(:get, /.*api\.bigcartel\.com.*/).to_return(lambda { |request|
       { :body => File.new(File.join(RSpec.configuration.fixture_path, 'store', request.uri.path.split('/', 3).last)), :status => 200, :headers => {} }

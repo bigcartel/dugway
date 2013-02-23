58a9a5ee6ec2b8101458a92c5a37ea1f0c4e55cc
Matt Wigham <matt@bigcartel.com>
2013-02-23 15:08:48 -0700

    Only override Time.parse on Ruby 1.9+

--- lib/extensions/time.rb
+++ lib/extensions/time.rb
@@ -1,12 +1,15 @@
 require 'time'
 
-class Time
-  class << self
-    # Support Ruby 1.8.7's Time.parse('Today')
-    alias :old_parse :parse
-    def parse(date, now=self.now)
-      date = now.to_s if date =~ /today/i
-      old_parse(date, now)
+# Support Ruby 1.8.7's Time.parse('Today')
+
+if RUBY_VERSION >= '1.9'
+  class Time
+    class << self
+      alias :old_parse :parse
+      def parse(date, now=self.now)
+        date = now.to_s if date =~ /today/i
+        old_parse(date, now)
+      end
     end
   end
 end

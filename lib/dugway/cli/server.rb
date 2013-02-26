11a68a9f8d1669468654d28048da7a43b04cd923
Matt Wigham <matt@bigcartel.com>
2013-02-26 06:40:11 -0700

    Provide daemonize option on server

--- lib/dugway/cli/server.rb
+++ lib/dugway/cli/server.rb
@@ -16,13 +16,20 @@ module Dugway
         :aliases => '-p',
         :default => 9292,
         :desc    => "The port address to bind to"
+
+      class_option :daemonize,
+        :type    => :boolean,
+        :aliases => '-d',
+        :default => false,
+        :desc    => "Run daemonized in the background"
       
       def start
         Rack::Server.start({
           :config => File.join(Dir.pwd, 'config.ru'),
           :environment => 'none',
           :Host => options[:host],
-          :Port => options[:port]
+          :Port => options[:port],
+          :daemonize => options[:daemonize]
         })
       end
     end

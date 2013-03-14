1660b1c61cd7e548f912f292db4d6d7cbe9ebb58
Matt Wigham <matt@bigcartel.com>
2013-03-14 16:59:01 -0600

    Finish drop specs. #1

--- spec/units/dugway/liquid/drops/products_drop_spec.rb
+++ spec/units/dugway/liquid/drops/products_drop_spec.rb
@@ -2,23 +2,97 @@ require 'spec_helper'
 
 describe Dugway::Drops::ProductsDrop do
   let(:order) { 'position' }
+  let(:params) { {} }
+  let(:registers) { { :params => params } }
 
-  let(:products) { 
+  let(:products) {
     Dugway::Drops::ProductsDrop.new(Dugway.store.products.map { |p| Dugway::Drops::ProductDrop.new(p) }).tap { |drop|
-      drop.context = Liquid::Context.new([{}, { 'internal' => { 'order' => order }}], {}, {})
+      drop.context = Liquid::Context.new([{}, { 'internal' => { 'order' => order }}], {}, registers)
     }
   }
 
   describe "#all" do
-    it "should return an array of all products"
+    it "should return an array of all products" do
+      all = products.all
+      all.should be_an_instance_of(WillPaginate::Collection)
+      all.size.should == 3
+
+      product = all.first
+      product.should be_an_instance_of(Dugway::Drops::ProductDrop)
+      product.name.should == 'My Product'
+    end
   end
 
   describe "#current" do
-    it "should return an array of all current products"
+    it "should return an array of current products" do
+      current = products.current
+      current.should be_an_instance_of(WillPaginate::Collection)
+      current.size.should == 3
+
+      product = current.first
+      product.should be_an_instance_of(Dugway::Drops::ProductDrop)
+      product.name.should == 'My Product'
+    end
+
+    describe "when filtering by artist" do
+      before(:each) do
+        registers.merge!(:artist => { 'permalink' => 'artist-one' })
+      end
+
+      it "should return an array of the artist's products" do
+        current = products.current
+        current.should be_an_instance_of(WillPaginate::Collection)
+        current.size.should == 1
+
+        product = current.first
+        product.should be_an_instance_of(Dugway::Drops::ProductDrop)
+        product.name.should == 'My Product'
+      end
+    end
+
+    describe "when filtering by category" do
+      before(:each) do
+        registers.merge!(:category => { 'permalink' => 'tees' })
+      end
+
+      it "should return an array of the category's products" do
+        current = products.current
+        current.should be_an_instance_of(WillPaginate::Collection)
+        current.size.should == 2
+
+        product = current.first
+        product.should be_an_instance_of(Dugway::Drops::ProductDrop)
+        product.name.should == 'My Product'
+      end
+    end
+
+    describe "when searching" do
+      before(:each) do
+        registers[:params].merge!(:search => 'my product')
+      end
+
+      it "should return an array of the category's products" do
+        current = products.current
+        current.should be_an_instance_of(WillPaginate::Collection)
+        current.size.should == 1
+
+        product = current.first
+        product.should be_an_instance_of(Dugway::Drops::ProductDrop)
+        product.name.should == 'My Product'
+      end
+    end
   end
 
   describe "#on_sale" do
-    it "should return an array of all on sale products"
+    it "should return an array of on sale products" do
+      current = products.on_sale
+      current.should be_an_instance_of(WillPaginate::Collection)
+      current.size.should == 1
+
+      product = current.first
+      product.should be_an_instance_of(Dugway::Drops::ProductDrop)
+      product.name.should == 'Print'
+    end
   end
 
   describe "#top_selling" do
@@ -40,7 +114,11 @@ describe Dugway::Drops::ProductsDrop do
   end
 
   describe "#permalink" do
-    it "should return the product by permalink"
+    it "should return the product by permalink" do
+      product = products.print
+      product.should be_an_instance_of(Dugway::Drops::ProductDrop)
+      product.name.should == 'Print'
+    end
 
     it "should return the nil for an invalid permalink" do
       products.blah.should be_nil
--- spec/units/dugway/store_spec.rb
+++ spec/units/dugway/store_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Dugway::Store do
   let(:store) { Dugway::Store.new('dugway') }
-  
+
   describe "#account" do
     it "should return account params" do
       store.account.should be_present
@@ -10,7 +10,7 @@ describe Dugway::Store do
       store.account['url'].should == 'http://dugway.bigcartel.com'
     end
   end
-  
+
   describe "#theme_pages" do
     it "should return an array of the default theme pages" do
       store.theme_pages.should be_present
@@ -18,7 +18,7 @@ describe Dugway::Store do
       store.theme_pages.first.should == { 'name' => 'Home', 'permalink' => 'home', 'url' => '/', 'category' => 'theme' }
     end
   end
-  
+
   describe "#custom_pages" do
     it "should return an array of the store's custom pages" do
       store.custom_pages.should be_present
@@ -26,7 +26,7 @@ describe Dugway::Store do
       store.custom_pages.first.should == { 'category' => 'custom', 'content' => "<p>We're really cool!</p>", 'permalink' => 'about-us', 'name' => 'About Us', 'id' => 95821979, 'url' => '/about-us' }
     end
   end
-  
+
   describe "#pages" do
     it "should return an array of the combined theme and custom pages" do
       store.pages.should be_present
@@ -35,21 +35,21 @@ describe Dugway::Store do
       store.pages.last.should == { 'category' => 'custom', 'content' => "<p>We're really cool!</p>", 'permalink' => 'about-us', 'name' => 'About Us', 'id' => 95821979, 'url' => '/about-us' }
     end
   end
-  
+
   describe "#page" do
     it "should return a theme page by permalink" do
       store.page('home').should == { 'name' => 'Home', 'permalink' => 'home', 'url' => '/', 'category' => 'theme' }
     end
-    
+
     it "should return a custom page by permalink" do
       store.page('about-us').should == { 'category' => 'custom', 'content' => "<p>We're really cool!</p>", 'permalink' => 'about-us', 'name' => 'About Us', 'id' => 95821979, 'url' => '/about-us' }
     end
-    
+
     it "should return nil with a bad permalink" do
       store.page('blah').should be_nil
     end
   end
-  
+
   describe "#categories" do
     it "should return an array of the store's categories" do
       store.categories.should be_present
@@ -57,29 +57,29 @@ describe Dugway::Store do
       store.categories.first.should == { 'permalink' => 'cds', 'name' => 'CDs', 'id' => 4615184, 'url' => '/category/cds' }
     end
   end
-  
+
   describe "#category" do
     it "should return a category by permalink" do
       store.category('cds').should == { 'permalink' => 'cds', 'name' => 'CDs', 'id' => 4615184, 'url' => '/category/cds' }
     end
-    
+
     it "should return nil with a bad permalink" do
       store.category('blah').should be_nil
     end
   end
-  
+
   describe "#category_products" do
     it "should return an array of products for that category" do
       products = store.category_products('tees')
       products.size.should == 2
       products.first['name'].should == 'My Product'
     end
-    
+
     it "should return an empty array with a bad permalink" do
       store.category_products('blah').should == []
     end
   end
-  
+
   describe "#artists" do
     it "should return an array of the store's artists" do
       store.artists.should be_present
@@ -87,29 +87,29 @@ describe Dugway::Store do
       store.artists.first.should == { 'permalink' => 'artist-one', 'name' => 'Artist One', 'id' => 176141, 'url' => '/artist/artist-one' }
     end
   end
-  
+
   describe "#artist" do
     it "should return an artist by permalink" do
       store.artist('artist-one').should == { 'permalink' => 'artist-one', 'name' => 'Artist One', 'id' => 176141, 'url' => '/artist/artist-one' }
     end
-    
+
     it "should return nil with a bad permalink" do
       store.artist('blah').should be_nil
     end
   end
-  
+
   describe "#artist_products" do
     it "should return an array of products for that artist" do
       products = store.artist_products('artist-one')
       products.size.should == 1
       products.first['name'].should == 'My Product'
     end
-    
+
     it "should return an empty array with a bad permalink" do
       store.artist_products('blah').should == []
     end
   end
-  
+
   describe "#products" do
     it "should return an array of the store's products" do
       store.products.should be_present
@@ -117,17 +117,17 @@ describe Dugway::Store do
       store.products.first['name'].should == 'My Product'
     end
   end
-  
+
   describe "#product" do
     it "should return a product by permalink" do
       store.product('my-product')['name'].should == 'My Product'
     end
-    
+
     it "should return nil with a bad permalink" do
       store.product('blah').should be_nil
     end
   end
-  
+
   describe "#product_and_option" do
     it "should a product and option by option id" do
       product, option = store.product_and_option(29474321)
@@ -152,7 +152,7 @@ describe Dugway::Store do
 
   describe "#next_product" do
     it "should return the next product when there is one" do
-      store.next_product('my-tee')['name'].should == 'My Print'
+      store.next_product('my-tee')['name'].should == 'Print'
     end
 
     it "should return nil when there isn't one" do
@@ -166,24 +166,24 @@ describe Dugway::Store do
       products.size.should == 1
       products.first['name'].should == 'My Product'
     end
-    
+
     it "should return an empty array with a bad permalink" do
       store.search_products('blah').should == []
     end
   end
-  
+
   describe "#country" do
     it "returns the store's country" do
       store.country.should == { 'name' => 'United States', 'id' => 43, 'code' => 'US' }
     end
   end
-  
+
   describe "#currency" do
     it "returns the store's currency" do
       store.currency.should == { 'sign' => '$', 'name' => 'U.S. Dollar', 'id' => 1, 'code' => 'USD' }
     end
   end
-  
+
   describe "#locale" do
     it "returns the store's locale based on their currency" do
       store.locale.should == 'en-US'

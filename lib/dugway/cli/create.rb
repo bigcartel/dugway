e9b5b98db4025478c0155156352ec4a7e971d482
Blake Walters <blake@markupboy.com>
2013-03-25 13:28:09 -0600

    added gemfile.lock template

--- lib/dugway/cli/create.rb
+++ lib/dugway/cli/create.rb
@@ -2,34 +2,35 @@ module Dugway
   module Cli
     class Create < Thor::Group
       include Thor::Actions
-      
+
       argument :name
-      
+
       class_option 'skip-source',
         :type    => :boolean,
         :default => false,
         :desc    => "Don't create a starter theme"
-      
+
       class_option 'skip-git',
         :type    => :boolean,
         :default => false,
         :desc    => "Don't create a .gitignore file"
-      
+
       class_option 'skip-bundle',
         :type    => :boolean,
         :default => false,
         :desc    => "Don't run bundle install"
-      
+
       def self.source_root
         File.join(File.dirname(__FILE__), 'templates')
       end
-      
+
       def core
         template('Gemfile.tt', "#{ theme_dir }/Gemfile")
+        template('GemfileLock.tt', "#{ theme_dir }/Gemfile.lock")
         template('config.tt', "#{ theme_dir }/config.ru")
         template('source/settings.json', "#{ source_dir }/settings.json")
       end
-      
+
       def source
         unless options['skip-source']
           Dir.glob("#{ self.class.source_root }/source/**/*.{html,jpg,png,js,coffee,css,sass}") do |file|
@@ -38,13 +39,13 @@ module Dugway
           end
         end
       end
-      
+
       def git
         unless options['skip-git']
           copy_file('gitignore.tt', "#{ theme_dir }/.gitignore")
         end
       end
-      
+
       def bundle
         unless options['skip-bundle']
           inside theme_dir, {} do
@@ -56,13 +57,13 @@ module Dugway
       def done
         say("#{ name } is ready!", :blue)
       end
-      
+
       private
-      
+
       def theme_dir
         name.parameterize
       end
-      
+
       def source_dir
         "#{ theme_dir }/source"
       end

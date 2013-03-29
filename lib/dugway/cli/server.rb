3ed57b7fdd60136ce6ed1de9d2b933c3704ac861
Matt Wigham <matt@bigcartel.com>
2013-03-29 09:47:11 -0600

    Change default server host to 127.0.0.1

--- lib/dugway/cli/server.rb
+++ lib/dugway/cli/server.rb
@@ -2,13 +2,13 @@ require 'rack'
 
 module Dugway
   module Cli
-    class Server < Thor::Group      
+    class Server < Thor::Group
       class_option :host,
         :type    => :string,
         :aliases => '-h',
-        :default => '0.0.0.0',
+        :default => '127.0.0.1',
         :desc    => 'The host address to bind to'
-      
+
       class_option :port,
         :type    => :numeric,
         :aliases => '-p',
@@ -19,8 +19,8 @@ module Dugway
         :type    => :string,
         :aliases => '-s',
         :default => 'thin',
-        :desc    => "Which server to run"
-      
+        :desc    => "The server to run"
+
       def start
         Rack::Server.start({
           :config => File.join(Dir.pwd, 'config.ru'),
